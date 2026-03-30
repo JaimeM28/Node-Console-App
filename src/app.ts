7559818c2cc8f9421eaad8ce81b7ac119f4a16dc
JaimeM28 <mirandaserranojaimemanuel@gmail.com>
2026-03-30 15:00:16 -0600

    feat: add yargs to manage CLI arguments

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,5 @@
-import fs from 'fs';
+import { yarg } from "./config/plugins/args.plugin";
 
-let outputMessage = '';
-const base = 5; 
-const headerMessage  =  `
-=======================================
-        Tabla del ${base}
-=======================================\n
-`;
+console.log(process.env)
 
-for(let i = 1; i <=10 ; i++){
-    outputMessage += `${base} x ${i} = ${base * i} \n`
-}
-
-outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
-
-const outputPath = `outputs/`; 
-fs.mkdirSync(outputPath, {recursive: true})
-fs.writeFileSync(`${outputPath}tabla-${base}.txt`, outputMessage);
+console.log(yarg.d)
