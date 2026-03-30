9d13417c5df7a12f65f2d156da1287ad0a862df3
JaimeM28 <mirandaserranojaimemanuel@gmail.com>
2026-03-30 15:40:36 -0600

    feat: add argumens base, limit and show

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import { yarg } from "./config/plugins/args.plugin";
 
-console.log(process.env)
+// console.log(process.env)
 
-console.log(yarg.d)
+// console.log(yarg.d)
+
+(async() =>{
+    await main();
+})();
+
+async function main() {
+    console.log(yarg.base)
+}
