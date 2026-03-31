a638b9014fa58f80ff065191592d74d9b2f1a7b9
JaimeM28 <mirandaserranojaimemanuel@gmail.com>
2026-03-31 13:48:54 -0600

    fix: delete whitespace

--- src/domain/use-cases/create-table.use-case.ts
+++ src/domain/use-cases/create-table.use-case.ts
@@ -16,7 +16,7 @@ export class CreateTable implements CreateTableUseCase{
     }
 
     execute({base, limit} : CreateTableOptions){
-        let outputMessage= ' '
+        let outputMessage= ''
         for(let i = 1; i <=limit ; i++){
             outputMessage += `${base} x ${i} = ${base * i} \n`
         }
